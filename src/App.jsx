c399167817dd77f51b6700bdc0aa55001af85045
Bolotnuk <nikita24tenith@gmail.com>
2026-01-26 11:08:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import MoviesPage from "./pages/MoviesPage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
-import MovieCast from "./components/MovieCast";
-import MovieReviews from "./components/MovieReviews";
-import NotFoundPage from "./pages/NotFoundPage";
+import { Suspense, lazy } from "react";
 import Navigation from "./components/Navigation";
+
+// 🔁 Динамічний імпорт сторінок
+const HomePage = lazy(() => import("./pages/HomePage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage"));
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
+const MovieCast = lazy(() => import("./components/MovieCast"));
+const MovieReviews = lazy(() => import("./components/MovieReviews"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 
 export default function App() {
   return (
     <>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<MovieCast />} />
-          <Route path="reviews" element={<MovieReviews />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<p style={{ padding: "1rem" }}>Loading...</p>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
